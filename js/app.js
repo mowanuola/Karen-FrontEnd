a7eaf4e99a3b3d94d5ceb07caaad7a64914ce9fb
Akin-Jimoh Mowanuola <akinjimohmowa@gmail.com>
2019-03-05 18:16:36 +0100

    Completed Front End Design

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  } 
+function toggleSideNav() {
+  const hamburger = document.querySelector("#hamburger");
+  const sideNav = document.querySelector("#sidenav");
+  hamburger.classList.toggle("change");
+  sideNav.classList.toggle("sidenav-open");
+}
