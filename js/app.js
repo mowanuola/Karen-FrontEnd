44295bcc6ea4df3589cf112cabf70d58376a161c
Akin-Jimoh Mowanuola <akinjimohmowa@gmail.com>
2019-03-13 10:55:34 +0100

    Modified front end application

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@ function toggleSideNav() {
   hamburger.classList.toggle("change");
   sideNav.classList.toggle("sidenav-open");
 }
+function registerServiceWorker() {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(reg => {
+        console.log(
+          "Service Worker Registration Successful with scope:",
+          reg.scope
+        );
+      })
+      .catch(error => {
+        console.log("Service Worker Registration Failed:", error);
+      });
+  }
+}
+registerServiceWorker();
