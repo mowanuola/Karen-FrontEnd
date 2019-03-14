8423b897a64bbecfb3d553a55d3afc346b1eb58c
Akin-Jimoh Mowanuola <akinjimohmowa@gmail.com>
2019-03-14 18:52:17 +0100

    Register page completed

---
+++ js/login.js
@@ -0,0 +1,41 @@
+const CONFIG = { API_URL: "http://localhost:8000" };
+async function login(data) {
+  try {
+    const response = await fetch(`${CONFIG.API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    if (response.status === 200) {
+      const json = await response.json();
+      localStorage.setItem("token", json.token);
+      window.location = "profile.html";
+    } else {
+      alert("Invalid login!");
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("An error occurred");
+    return;
+  }
+}
+
+const loginForm = document.querySelector("#login-form");
+console.log(loginForm);
+const loginButton = document.querySelector("#login-button");
+// loginForm.onsumbit = async e => {
+
+// };
+loginForm.addEventListener("submit", async event => {
+  event.preventDefault();
+  loginButton.setAttribute("disabled", true);
+  const data = {
+    username: loginForm.username.value,
+    password: loginForm.password.value
+  };
+  await login(data);
+  loginButton.setAttribute("disabled", false);
+});
---
+++ js/register.js
@@ -0,0 +1,47 @@
+const CONFIG = { API_URL: "http://localhost:8000" };
+async function register(data) {
+  try {
+    const response = await fetch(`${CONFIG.API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    if (response.status === 201) {
+      const json = await response.json();
+      localStorage.setItem("token", json.token);
+      window.location = "login.html";
+    } else {
+      alert("Invalid registration!");
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("An error occurred");
+    return;
+  }
+}
+
+const registerForm = document.querySelector("#register-form");
+console.log(registerForm);
+const registerButton = document.querySelector("#register-button");
+// registerForm.onsumbit = async e => {
+
+// };
+registerForm.addEventListener("submit", async event => {
+  event.preventDefault();
+  registerButton.setAttribute("disabled", true);
+  const data = {
+    username: registerForm.username.value,
+    password: registerForm.password.value,
+    first_name: registerForm.firstname.value,
+    last_name: registerForm.lastname.value,
+    birth_date: registerForm.dob.value,
+    email: registerForm.email.value,
+    sex: registerForm.sex.value,
+   
+  };
+  await register(data);
+  registerButton.setAttribute("disabled", false);
+});
